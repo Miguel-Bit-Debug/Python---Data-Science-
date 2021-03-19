95c4474fe22712ea9caaf0e40da22c959a67f629
Miguel dos Santos Ribeiro <migueldossantos011@gmail.com>
2021-03-19 13:15:32 -0300

    add scripts - tuple

--- operacoes-com-dicionarios.py
+++ operacoes-com-dicionarios.py
@@ -6,5 +6,23 @@ print(dados.values())
 dados['DS5'] = 124549.07
 print(dados)
 del dados['Passat']
-dados.pop("Crossfox")
 print(dados)
+
+dados.update({'Passat': 106161.94})
+dados.update({'Passat': 106161.95})
+print(dados)
+dados.update(({'Fusca': 106161.94}))
+print(dados)
+
+dadosCopy = dados.copy()
+print(f'dados copy{dadosCopy}')
+del dadosCopy['Fusca']
+print(f'dados copy{dadosCopy}')
+print(f'dados {dados}')
+
+
+dadosCopy.pop("Passat")
+print(f'dados copy{dadosCopy}')
+
+dadosCopy.clear()
+print(dadosCopy)
