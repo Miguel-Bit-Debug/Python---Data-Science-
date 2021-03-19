838b51f31699af5982977abcda1adc3573142a6b
Miguel dos Santos Ribeiro <migueldossantos011@gmail.com>
2021-03-19 11:40:55 -0300

    add scripts - tuple

--- tuplas.py
+++ tuplas.py
@@ -6,3 +6,16 @@ preco = (nome, valor)
 
 nomes_carros = tuple(['Jetta Variant', 'Passat', 'Crossfox', 'DS5'])
 type(nomes_carros)
+
+
+# ---------------------------- #
+
+# selecao em tuplas
+
+nomes_carros2 = ('Jetta Variant', 'Passat', 'Crossfox', 'DS5', ('Fusca', 'Gol', 'C4'))
+# print(nomes_carros[0])
+# print(nomes_carros[1])
+# print(nomes_carros[2])
+# print(nomes_carros[-1])
+# print(nomes_carros[0:4:2])
+print(nomes_carros2[-1][1])
