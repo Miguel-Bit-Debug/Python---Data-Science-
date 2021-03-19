d03afe3c66356dec38afcdf2ce4c7fd366ebbfee
Miguel dos Santos Ribeiro <migueldossantos011@gmail.com>
2021-03-19 11:13:55 -0300

    add scripts

---
+++ numpy-1.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+dados = np.array([[4410., 37123., 0., 25757.],
+                 [2003., 1991., 2019, 2006. ]])
+
+
+print(dados[:, :] / (2021 - dados[:, :]))
+
+
+print(0)
+print("-----------------------")
+
+print(dados.dtype)
+print(dados[:, :][:])
