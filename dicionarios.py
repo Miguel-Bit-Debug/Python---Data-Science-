cc03d771a18a4747915285b26796de2b0a19d7ca
Miguel dos Santos Ribeiro <migueldossantos011@gmail.com>
2021-03-19 12:13:14 -0300

    add scripts - tuple

---
+++ dicionarios.py
@@ -0,0 +1,8 @@
+carros = ['Jetta Variant', 'Passat', 'Crossfox']
+
+dados = {'Jetta Variant': 88078.64, 'Passat': 106161.94, 'Crossfox': 72832.16}
+valores = [88078.64, 106161.94, 72832.16]
+
+type(dados)
+
+print(list(zip(carros, valores)))
