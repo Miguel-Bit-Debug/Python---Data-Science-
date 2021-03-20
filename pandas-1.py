dfd2cbff3192121e10a042f96eef95c6f54741b4
Miguel dos Santos Ribeiro <migueldossantos011@gmail.com>
2021-03-20 02:04:29 -0300

    add scripts - pandas

---
+++ pandas-1.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+carros = ['Jetta Variant', 'Passat', 'Crossfox']
+
+# dataset = pd.DataFrame(carros)
+
+data = [
+     {'Nome': 'Jetta Variant', 'Motor': 'Motor 4.0 Turbo', 'Ano': 2003, 'Quilometragem': 44410.0, 'Zero_km': False, 'Valor': 88078.64},
+     {'Nome': 'Passat', 'Motor': 'Motor Diesel', 'Ano': 1991, 'Quilometragem': 5712.0, 'Zero_km': False, 'Valor': 106161.94},
+     {'Nome': 'Crossfox', 'Motor': 'Motor Diesel V8', 'Ano': 1990, 'Quilometragem': 37123.0, 'Zero_km': False, 'Valor': 72832.16}
+ ]
+
+# dataset = pd.DataFrame(data)
+
+dados = {
+    'Nome': ['Jetta Variant', 'Passat', 'Crossfox'],
+    'Motor': ['Motor 4.0 Turbo', 'Motor Diesel', 'Motor Diesel V8'],
+    'Ano': [2003, 1991, 1990],
+    'Quilometragem': [44410.0, 5712.0, 37123.0],
+    'Zero_km': [False, False, False],
+    'Valor': [88078.64, 106161.94, 72832.16]
+}
+
+dataset = pd.DataFrame(dados)
+
+print(dataset)
+# print(pd.Series(carros))
